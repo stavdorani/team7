08d88126cbe3154ebbf9260e9ca4c86face0a72b
stavdorani <85304499+stavdorani@users.noreply.github.com>
2022-05-29 01:21:24 +0300

    PartB

--- static/js/login-js.js
+++ static/js/login-js.js
@@ -1,8 +1,8 @@
-function myFunction(idP) {
-    var x = document.getElementById(idP);
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
+function myFunction() {
+  var x = document.getElementById("psw");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
   }
+}
